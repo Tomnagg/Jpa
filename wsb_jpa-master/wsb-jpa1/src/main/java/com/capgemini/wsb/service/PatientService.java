d42bdd5381fdc99417c92019f3a949e05f0be098
Tomnagg <naglaktomasz@gmail.com>
2025-06-08 14:58:27 +0200

    Delete wsb_jpa-master directory

--- wsb_jpa-master/wsb-jpa1/src/main/java/com/capgemini/wsb/service/PatientService.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.wsb.service;
-
-import com.capgemini.wsb.dto.PatientTO;
-
-public interface PatientService {
-
-    PatientTO findById(Long id);
-    void deletePatient(Long id);
-}
